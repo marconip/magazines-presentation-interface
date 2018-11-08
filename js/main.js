8635eb7fd9a1360eef7a6d5156329fcf02278cfc
marconip <marconidesenhos@gmail.com>
2018-11-08 17:08:20 -0200

    first files

---
+++ js/main.js
@@ -0,0 +1,38 @@
+//MOSTRA IMAGEM SELECIONADA
+function showhide(el) {
+    var display = document.getElementById(el).style.display;
+    if (display == "block")
+        document.getElementById(el).style.display = 'none';
+    else
+        document.getElementById(el).style.display = 'block';
+};
+
+//ADICIONA CLASSE MOSTRANDO O SELECIONADO
+$('.selected').click(function () {
+    $(this).toggleClass('active');
+
+    //AVISO DE QUANTIDADES SELECIONADAS
+    var maxSelecionado = document.querySelectorAll(".active");
+    if (maxSelecionado.length == 3) {
+        alert("JÁ TEMOS 2 SELECIONADOS! \nMas pode continuar selecionando")
+    }
+});
+
+//MODAL PARA IMAGENS AMPLIADAS
+var cliq = document.querySelectorAll(".imgclick");
+cliq.forEach(function (img) {
+    img.onclick = function () {
+        document.getElementById("modal").style.display = "block";
+        document.getElementById("modal-img").src = this.src;
+    }
+});
+
+//LINK PARA IR AO TOPO SUAVEMENTE
+$("#gototop").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 900);
+    return false;
+});
+
+//PREÇOS MASCARA
+//http://igorescobar.github.io/jQuery-Mask-Plugin/docs.html
+$('.money').mask('#.##0,00', { reverse: true });
